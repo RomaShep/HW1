a19960332d0ac84a45d9de5ed9da4cf47f506b80
Roman <sxolastika@gmail.com>
2016-02-28 01:51:29 +0200

    HW4 4 commit

--- main.rb
+++ main.rb
@@ -1,6 +1,6 @@
 class Developer
-  attr_reader :name, :task_array, :TYPE, :STYPE
-  attr_accessor :priority 
+  attr_reader :name, :task_array
+  attr_accessor :GROUP, :TYPE
   
   MAX_TASKS = 10
   GROUP = :developers
@@ -9,18 +9,17 @@ class Developer
   def initialize(name)
     @name = name
     @task_array = []
-    @priority = 999
   end
 
-  def MAX_TASKS
+  def max_tasks
     self.class::MAX_TASKS
   end
 
-  def GROUP
+  def group
     self.class::GROUP
   end
 
-  def TYPE
+  def type
     self.class::TYPE
   end
 
@@ -51,7 +50,7 @@ class Developer
   end
 
   def can_add_task?
-    @task_array.count < self.class::MAX_TASKS
+    @task_array.count < max_tasks
   end
 
   def can_work?
@@ -97,119 +96,96 @@ class SeniorDeveloper < Developer
 end
 
 class Team
-  #attr_accessor :seniors, :developers, :juniors
-  attr_accessor :prior, :team
+  attr_accessor :Priority, :team
 
-  def initialize
+  def initialize(&block)
     @team = []  
-    @prior = []                    
+    @Priority = [:juniors, :developers ,:seniors] 
+    if block 
+      instance_eval &block 
+    end
   end                               
     
   def have_seniors(*dev_name)     dev_name.map{ |name| @team << SeniorDeveloper.new(name)} end
   def have_developers(*dev_name)  dev_name.map{ |name| @team << Developer.new(name)} end
   def have_juniors(*dev_name)     dev_name.map{ |name| @team << JuniorDeveloper.new(name)} end
   
-  def seniors()     @team.select{ |dev| dev.TYPE == :senior}  end
-  def developers () @team.select{ |dev| dev.TYPE == :developer} end
-  def juniors ()    @team.select{ |dev| dev.TYPE == :junior} end
+  def seniors()     @team.select{ |dev| dev.type == :senior}  end
+  def developers () @team.select{ |dev| dev.type == :developer} end
+  def juniors ()    @team.select{ |dev| dev.type == :junior} end
 
   def all() @team end
 
-  def priority (*prior)
-    @prior = *prior
-    # @prior.map.with_index{ |x,i| puts "#{x} -- #{i}"}
-    # @team.map { |dev| puts "#{dev.name} == #{dev.MAX_TASKS}"}
-    @team.map { |dev| @prior.map.with_index{ |priority,i| dev.priority = i if dev.GROUP == priority }}
+  def priority (*priority_list)
+    @Priority = *priority_list
   end
 
-  def on_task(developer, &block)
-    on_task_developer[developer] = block
+  def on_task(method, &block)
+    on_task_blocks[method] = block
+  end
+  def on_task_blocks
+    @on_task_blocks ||= {} 
   end
 
-  def on_task_developer
-    @on_task_developer ||= {}
-  end
 
-  # def on_task(options={}, proc)
-  #   proc.call
-  # end 
-  
-  # def add_task (task)
-  #   sort_team.first.add_task(task)
-  # end 
-  def add_task (task_list, options={})
-    # if !options[:complexity].nil? and !options[:to].nil?
-    #   task_list.map do |task| 
-    #     a=sort_team.select{|dev| dev.TYPE == options[:complexity] && dev.name == options[:to]}
-    #     unless a.empty?
-    #       a.first.add_task(task)
-    #     else
-    #       raise "Нет разработчика: #{options[:to]} уровня - #{options[:complexity]}"
-    #     end
-    #   end 
-    # elsif !options[:complexity].nil?
-    #   task_list.map do |task| 
-    #     a=sort_team.select{|dev| dev.TYPE == options[:complexity]}
-    #     unless a.empty?
-    #       a.first.add_task(task)
-    #     else
-    #       raise "Нет разработчика уровня - #{options[:complexity]}"
-    #     end
-    #   end
-    # elsif !options[:to].nil?
-    #   task_list.map do |task| 
-    #     a=sort_team.select{|dev| dev.name == options[:to]}
-    #     unless a.empty?
-    #       a.first.add_task(task)
-    #     else
-    #       raise "Нет разработчика: #{options[:to]}"
-    #     end
-    #   end
-    # else      
-      task_list.map{|task| sort_team.first.add_task(task)}
-    #end
+  def add_task (task, options={})
+
+    if !options[:complexity].nil? and !options[:to].nil?
+      a=sort_team.select{|dev| dev.type == options[:complexity] && dev.name == options[:to]}.first
+      raise "Нет разработчика: #{options[:to]} уровня - #{options[:complexity]}" if a.nil?
+    elsif !options[:complexity].nil?
+      a=sort_team.select{|dev| dev.type == options[:complexity]}.first
+      raise "Нет разработчика уровня - #{options[:complexity]}" if a.nil?
+    elsif !options[:to].nil?
+      a=sort_team.select{|dev| dev.name == options[:to]}.first
+      raise "Нет разработчика: #{options[:to]}" if a.nil?
+    else      
+      a=sort_team.first
+      raise "Нет разработчиков!!!}" if a.nil?
+    end
+
+    a.add_task(task)
+    after = on_task_blocks[a.type]
+    if after
+      after.call(a,task)
+    end
   end 
 
   def report
-    sort_team.map{|dev| puts "#{dev.name} (#{dev.TYPE}): #{dev.task_array.map{|x| x}.join(", ")} "} #{dev.priority} -- #{!dev.can_add_task?}"} 
+    sort_team.map{|dev| puts "#{dev.name} (#{dev.type}): #{dev.task_array.map{|x| x}.join(", ")} "}
+    #sort_team.map{|dev| puts "#{dev.name} (#{dev.type}): #{dev.task_array.count} -- priority -- #{@prior.index(dev.groupe)} -- Can add task: #{dev.can_add_task?} -- #{dev.MAX_TASKS } "} 
+    
   end
   def sort_team
-    @team.sort_by{|dev| [dev.task_array.count, dev.priority]  }.sort{|a,b|  (a.can_add_task? == b.can_add_task?) ? 0 : (a.can_add_task? ? -1 : 1)}
-    #@team.sort_by{|dev| [dev.task_array.count, dev.priority]}.sort{|a,b|  (a.can_add_task? == b.can_add_task?) ? 0 : (a.can_add_task? ? -1 : 1)}
+    @team.select{|dev| !@Priority.index(dev.group).nil? }.sort_by{|dev|  [(dev.can_add_task? ? -1 : 1),dev.task_array.count, @Priority.index(dev.group)]}
   end
 end
 
-t = Team.new()
-t.have_seniors 'SDev1', 'SDev2', 'Ivan'
-t.have_developers 'Dev1', 'Dev2', 'Ivan'
-t.have_juniors 'JDev1', 'JDev2', 'JDev3'
-t.priority  :developers, :juniors #,:seniors 
-# t.seniors.map{ |dev| puts dev.class}
-#p t.all.map{ |dev| puts dev[:dev].name}
-# puts t.seniors
-# puts t.prior
-puts "*****Report*******"
+t = Team.new do
+  have_seniors 'SDev1', 'SDev2'
+  have_developers 'Dev1', 'Dev2'
+  have_juniors 'JDev1', 'JDev2', 'JDev3'
+  priority  :juniors, :developers ,:seniors 
+
+  on_task :junior do |dev,task|
+    puts "Отдали задачу #{task} разработчику #{dev.name}, следите за ним!"
+  end
+
+  on_task :senior do |dev, task|
+    puts "#{dev.name} сделает #{task}, но просит больше с такими глупостями не приставать"
+  end
+end
+
+
+#p t.seniors
+#puts "*****Report*******"
 #t.report
-t.add_task ['T1','t2'],  to: 'Ivan'
-# 60.times{ |i| t.add_task "T#{i}"}
+#t.add_task ['T1','t2'],  to: 'Ivan'
+14.times{ |i| t.add_task "T#{i}" }#, complexity: :senior, to: 'SDev1'}
 t.report
+#t.report
 # p t.juniors
 
-# jun = JuniorDeveloper.new('Sid')
-# p jun.MAX_TASKS
-# puts t.seniors
-# puts t.developers
-# puts t.juniors
-# dev = SeniorDeveloper.new('Sen')
-# p dev
-# puts dev.status
-# dev.add_task("a")
-# dev.add_task("b")
-# dev.add_task("d")
-# dev.add_task("c")
-# puts dev.can_add_task?
-# puts dev.can_work?
-# dev.work!
-# puts dev.tasks
+
 
 
